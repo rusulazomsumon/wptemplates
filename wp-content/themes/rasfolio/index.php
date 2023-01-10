b937fb4209fd12766e0608b034dc5cbe5f318b5c
RusulAzomSumon <18103020@iubat.edu>
2023-01-10 21:47:23 +0600

    notification banner updated

--- wp-content/themes/rasfolio/index.php
+++ wp-content/themes/rasfolio/index.php
@@ -51,13 +51,27 @@
     <!-- intro area end -->
 
       <!-- #####################Notification Banner################### -->
-        <div class="notification-banner">
-          <img src="<?php echo get_template_directory_uri(); ?>/assect/img/otw.png" height="50%" width="auto" alt="Notification banner">
-          <div class="notification-banner-buttons">
-            <a href="#" class="button close">Open CV</a>
-            <a href="#" class="button close">Close</a>
+      <div class="notification-banner">
+        <!-- banner texts -->
+          <div class="text-area">
+            <div class="banner-text">
+              <h1>Open To Work</h1>
+              <p>I'm Lokking for a Internship as a WordPress Developer/PHP Junior Developer</p>
+            </div>
+            <div class="notification-banner-buttons">
+              <a href="#" class="button opnCv">Open CV</a>
+              <a href="#" class="button mail">Send Mail</a>
+            </div>
           </div>
-        </div>
+          <!-- close button -->
+          <div class="close">
+            <a href="#">
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
+</svg>
+            </a>
+          </div>
+      </div>
 
       <script>
         document.addEventListener( 'DOMContentLoaded', function() {
@@ -75,8 +89,15 @@
             // hide the banner
             banner.style.display = 'none';
           });
+
+          // hide the banner after 10 seconds
+          setTimeout(function() {
+            var banner = document.querySelector( '.notification-banner' );
+            banner.style.display = 'none';
+          }, 10000);
         });
       </script>
+
 
     <!-- #################################Blog Area################################### -->
     <section class="container-flued blog-area">
